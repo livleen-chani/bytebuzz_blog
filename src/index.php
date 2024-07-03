7fc249e52751911315d7b13d833f925306a3f67f
livleensinghchani <livleensinghchani@gmail.com>
2024-07-03 16:22:51 +0530

    Fix

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "db";
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php echo "Hey User"; ?>
+</body>
+</html>
